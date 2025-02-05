83de93da02e08c0568121340ae0b06fc8c225255
Rinels <serg.kuzin.sk@gmail.com>
2025-02-05 23:59:30 +0300

    Первый уровень

---
+++ tiles2.thumb.png.65dffda5d728405d4cf56e14a9d31e18.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tiles2.thumb.png.65dffda5d728405d4cf56e14a9d31e18" tilewidth="32" tileheight="32" tilecount="465" columns="31">
+ <image source="tiles2.thumb.png.65dffda5d728405d4cf56e14a9d31e18.png" width="1000" height="510"/>
+</tileset>
